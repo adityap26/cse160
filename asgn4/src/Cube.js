9e43ad9f327caa9a82fdb2f53b6a84f0d1e8e73f
Aditya Patil <adityapatil1826@gmail.com>
2026-02-23 12:38:30 -0800

    asgn4 set up

---
+++ asgn4/src/Cube.js
@@ -0,0 +1,48 @@
+// Cube.js
+
+let g_cubeInterleavedVBO = null;
+let g_cubeVertexCount = 0;
+
+function initCubeBuffers(gl) {
+  const data = new Float32Array([
+    // +X
+    0.5, -0.5, -0.5, 0, 0,  0.5,  0.5, -0.5, 1, 0,  0.5,  0.5,  0.5, 1, 1,
+    0.5, -0.5, -0.5, 0, 0,  0.5,  0.5,  0.5, 1, 1,  0.5, -0.5,  0.5, 0, 1,
+    // -X
+   -0.5, -0.5,  0.5, 0, 0, -0.5,  0.5,  0.5, 1, 0, -0.5,  0.5, -0.5, 1, 1,
+   -0.5, -0.5,  0.5, 0, 0, -0.5,  0.5, -0.5, 1, 1, -0.5, -0.5, -0.5, 0, 1,
+    // +Y
+   -0.5,  0.5, -0.5, 0, 0, -0.5,  0.5,  0.5, 0, 1,  0.5,  0.5,  0.5, 1, 1,
+   -0.5,  0.5, -0.5, 0, 0,  0.5,  0.5,  0.5, 1, 1,  0.5,  0.5, -0.5, 1, 0,
+    // -Y
+   -0.5, -0.5,  0.5, 0, 0, -0.5, -0.5, -0.5, 0, 1,  0.5, -0.5, -0.5, 1, 1,
+   -0.5, -0.5,  0.5, 0, 0,  0.5, -0.5, -0.5, 1, 1,  0.5, -0.5,  0.5, 1, 0,
+    // +Z
+   -0.5, -0.5,  0.5, 0, 0,  0.5, -0.5,  0.5, 1, 0,  0.5,  0.5,  0.5, 1, 1,
+   -0.5, -0.5,  0.5, 0, 0,  0.5,  0.5,  0.5, 1, 1, -0.5,  0.5,  0.5, 0, 1,
+    // -Z
+    0.5, -0.5, -0.5, 0, 0, -0.5, -0.5, -0.5, 1, 0, -0.5,  0.5, -0.5, 1, 1,
+    0.5, -0.5, -0.5, 0, 0, -0.5,  0.5, -0.5, 1, 1,  0.5,  0.5, -0.5, 0, 1
+  ]);
+
+  g_cubeVertexCount = 36;
+  g_cubeInterleavedVBO = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, g_cubeInterleavedVBO);
+  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+}
+
+function drawCube(gl, attrs, uniforms, modelMatrix, baseColor, textureNum, texColorWeight) {
+  gl.uniformMatrix4fv(uniforms.u_ModelMatrix, false, modelMatrix.elements);
+  gl.uniform4f(uniforms.u_FragColor, baseColor[0], baseColor[1], baseColor[2], baseColor[3]);
+  gl.uniform1i(uniforms.u_whichTexture, textureNum);
+  gl.uniform1f(uniforms.u_texColorWeight, texColorWeight);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, g_cubeInterleavedVBO);
+  gl.vertexAttribPointer(attrs.a_Position, 3, gl.FLOAT, false, 5 * 4, 0);
+  gl.enableVertexAttribArray(attrs.a_Position);
+  gl.vertexAttribPointer(attrs.a_UV, 2, gl.FLOAT, false, 5 * 4, 3 * 4);
+  gl.enableVertexAttribArray(attrs.a_UV);
+
+  gl.drawArrays(gl.TRIANGLES, 0, g_cubeVertexCount);
+}
+  
